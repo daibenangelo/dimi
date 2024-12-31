9719ef9ad43ba776ad463b1f7c9e0a8e7205b9c2
daibenangelo <daibenangelosanchez@gmail.com>
2024-12-31 21:30:21 +0800

    trying again to tell the bot to be conservative with doc choices

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ async function selectDocuments(userQuery) {
       Here is a list of documents:
       ${documents.map((doc) => `- ${doc}`).join("\n")}
 
-      Select the documents that are most relevant to the user's query. Select as few as possible.
+      Only select the documents that are most relevant to the user's query.
+      Select as few documents as possible.
       Return a JSON object with the following structure:
       {
         "selectedFileNames": ["doc1.txt", "doc3.txt"]
