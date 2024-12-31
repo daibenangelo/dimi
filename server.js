49b17f13d8b984bcf919cb6d1e853b7c2a8d7aba
daibenangelo <daibenangelosanchez@gmail.com>
2024-12-31 19:11:30 +0800

    url encoding

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/js", express.static(path.join(__dirname, "js")));
 
 // Fetch all document names dynamically
