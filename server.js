75d5f3466d78a695abd6eed3e1796107762f02ae
daibenangelo <daibenangelosanchez@gmail.com>
2024-12-31 21:00:50 +0800

    changing env

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ async function selectDocuments(userQuery) {
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          Authorization: `Bearer ${process.env.API_KEY}`,
           "Content-Type": "application/json",
         },
       }
